b2f6348a41d8d83d79824666c9bd0a152e56bb0e
Danya Gavrylovskyi <danya.gavrilovskiy28@gmail.com>
2023-06-04 14:01:54 +0300

    Added authentication with Google in admin panel

--- Admin/nextdevices-admin/pages/index.tsx
+++ Admin/nextdevices-admin/pages/index.tsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import { Inter } from 'next/font/google';
+import { useSession, signIn, signOut } from 'next-auth/react';
 
 const inter = Inter({ subsets: ['latin'] });
 
 const Home: React.FC = () => {
-  return (
-    <div className="bg-blue-500 w-screen h-screen flex items-center">
-      <div className="text-center w-full">
-        <button className="bg-white p-2 px-4 rounded-lg">Login with Google</button>
+  const { data: session } = useSession();
+  if (!session) {
+    return (
+      <div className="bg-blue-500 w-screen h-screen flex items-center">
+        <div className="text-center w-full">
+          <button
+            onClick={() => signIn('google')}
+            className="bg-white p-4 px-6 rounded-2xl font-bold">
+            Login with Google
+          </button>
+        </div>
       </div>
+    );
+  }
+  return (
+    <div>
+      <span>Signed in as {session.user?.email}</span>
+      <button onClick={() => signOut()}>Sign out</button>
     </div>
   );
 };
