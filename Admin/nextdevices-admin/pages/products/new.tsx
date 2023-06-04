3df4418b5b84bc64dd094c94dd0f8a2730b1ec48
Danya Gavrylovskyi <danya.gavrilovskiy28@gmail.com>
2023-06-04 18:52:28 +0300

    Made layout for adding new product page

--- Admin/nextdevices-admin/pages/products/new.tsx
+++ Admin/nextdevices-admin/pages/products/new.tsx
@@ -4,20 +4,21 @@ import { Layout } from '../components/Layout';
 const NewProduct: React.FC = () => {
   return (
     <Layout>
-      <div className="mt-1 ml-10 text-blue-900 text-xl">
-        <h1 className="text-2xl mb-6 font-bold">New Product</h1>
-        <section>
+      <div className="mt-1 mx-10 text-blue-900 text-xl">
+        <h1 className="text-2xl font-bold mb-20">New Product</h1>
+        <section className="mb-6">
           Product name<br></br>
           <input placeholder="product name"></input>
         </section>
-        <section>
+        <section className="mb-6">
           Description<br></br>
-          <input placeholder="description"></input>
+          <textarea placeholder="description" className="h-10"></textarea>
         </section>
-        <section>
+        <section className="mb-20">
           Price<br></br>
           <input placeholder="price"></input>
         </section>
+        <button className="bg-blue-900 py-1 px-6 rounded-lg text-white mb-4">Save</button>
       </div>
     </Layout>
   );
